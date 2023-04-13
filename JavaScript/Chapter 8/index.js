ba414112bc636c20a98063b54287fd4507b0630f
SouravSaxena370 <souravsaxena303200@gmail.com>
2023-04-14 01:11:57 +0530

    Chapter 8 completed

--- JavaScript/Chapter 8/index.js
+++ JavaScript/Chapter 8/index.js
@@ -3,3 +3,16 @@ for(let i=0;i<btn.length;i++)
 {
     btn[i].addEventListener("click",()=>{alert(`Button${i+1} clicked`);});
 }
+
+button=document.getElementById("website-button");
+website=document.getElementById("website-name").value;
+link=document.getElementById("website-link").value;
+
+websites=document.getElementsByClassName("favourite-website")[0];
+item=localStorage.getItem("websites");
+button.addEventListener("click",()=>{
+    localStorage.setItem(website,link);
+    elem=document.createElement("p");
+    elem.innerText=`${website} : ${link}`;
+    websites.appendChild(elem);
+})
