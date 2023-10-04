885fb5a231e4f289d2f8c65ad54a49809aa8ffa5
SouravSaxena370 <souravsaxena303200@gmail.com>
2023-10-04 20:12:19 +0530

    changes update

---
+++ TalkSphere/frontend/src/Pages/HomePage.js
@@ -0,0 +1,53 @@
+import { Container,Box,Text,Tab,TabList,TabPanel,TabPanels,Tabs } from '@chakra-ui/react'
+import React from 'react'
+
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup'
+
+const HomePage = () => {
+  return (
+    <>
+    <Container maxW='xl' centerContent>
+    <Box
+        d="flex"
+        justifyContent="center"
+        textAlign={"center"}
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        boarderWidth="1px"
+        >
+            <Text fontSize="4xl" fontFamily={"Work sans"} color="black">TalkSphere</Text>
+        </Box>
+        <Box d="flex"
+        justifyContent="center"
+        textAlign={"center"}
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        boarderWidth="1px">
+            <Tabs variant='soft-rounded' >
+  <TabList mb="1em">
+    <Tab width="50%">Login</Tab>
+    <Tab width="50%">Sign Up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <Login/>
+    </TabPanel>
+    <TabPanel>
+      <Signup/>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+        </Box>
+    </Container>
+    </>
+  )
+}
+
+export default HomePage
